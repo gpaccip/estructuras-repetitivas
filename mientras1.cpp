41c432b29b40a848a13fd047b3ac48589c0052b5
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-18 00:24:33 -0500

    actualizacion 1 del ejercicio mientras1.cpp

--- mientras1.cpp
+++ mientras1.cpp
@@ -1,21 +1,23 @@
 //Pacci Perez Giorgini Disalvatore
 //mientras1.cpp
 #include<iostream>
+#include<stdlib.h>
  using namespace std;
   int main(){
     int n,edad,i,s=0;
     
+    cout<<"\tCONTEO DE TODOS LOS ESTUDIANTES "<<endl;
     cout<<"Digite el numero de estudiantes: ";cin>>n;
     i=1;
     
     while (i<=n){
-    cout<<"Ingresa la edad de la persona "<<i<<": ";cin>>edad;
-    s=s+edad;
-    i=i+1;
+    	cout<<"Ingresa la edad de la persona "<<i<<": ";cin>>edad;
+    	s=s+edad;
+    	i=i+1;
     }
 
     cout<<"La suma de las edades es: "<<s<<endl;
     
-    
+    system("pause");
   	return 0;
-  }
+ }
