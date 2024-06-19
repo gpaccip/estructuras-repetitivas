b64a3602fb6c23c887d31e111f6023415f138a96
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-18 23:41:15 -0500

    actualizacion 2 del ejercicio mientras1.cpp

--- mientras1.cpp
+++ mientras1.cpp
@@ -4,20 +4,22 @@
 #include<stdlib.h>
  using namespace std;
   int main(){
-    int n,edad,i,s=0;
+    int n,edad,i=1,s=0;
     
-    cout<<"\tCONTEO DE TODOS LOS ESTUDIANTES "<<endl;
-    cout<<"Digite el numero de estudiantes: ";cin>>n;
-    i=1;
+    cout<<"\tCONTEO Y LA SUMA TOTAL DE TODAS LAS EDADES DE LOS ESTUDIANTES "<<endl;
+    cout<<"Digite la cantidad de estudiantes: ";cin>>n;
     
     while (i<=n){
     	cout<<"Ingresa la edad de la persona "<<i<<": ";cin>>edad;
     	s=s+edad;
     	i=i+1;
     }
-
-    cout<<"La suma de las edades es: "<<s<<endl;
-    
+    if(n>1){
+    	cout<<"La suma total de todas las edades es: "<<s<<endl;
+	}
+	else{
+		cout<<"La edad del estudiante es: "<<s<<endl;
+	}
     system("pause");
   	return 0;
  }
