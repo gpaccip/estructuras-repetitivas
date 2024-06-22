9985cd77842adbc28cd9b2589fa8b07471d9deb0
gpaccip <gpaccip@unjbg.edu.pe>
2024-06-21 23:57:50 -0500

    actualizacion 5 del ejercicio mientras1.cpp

--- mientras1.cpp
+++ mientras1.cpp
@@ -14,19 +14,21 @@
     		cout<<"Ingrese la edad del estudiante "<<i<<": ";cin>>edad;
     			s=s+edad;
     			i++;
-   			 }
-   			if(n>1){
+   				 }
+   				if(n>1){
     			cout<<endl<<"La suma de las edades es: "<<s<<endl;
-			}
+				}
 		}
-				else if(n=1){	
+		else if(n=1){	
 					cout<<"Ingrese la edad del unico estudiante: ";cin>>edad;
 					cout<<endl<<"Por ser unico estudiante la suma total del salon es su edad, que es: "<<edad<<endl;
 				}
-					else{
+			else if(n=0){	
+					cout<<"No hay estudiantes en el salon ";
+				}
+				else{
 					cout<<"La cantidad de estudiantes no puede ser negativa ";
 					}
-		
     system("pause");
   	return 0;
  }
